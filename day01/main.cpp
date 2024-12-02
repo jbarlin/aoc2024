1920d613153c45f8960faeb70f5ed941b23bb7ba
Conrad <41846367+jbarlin@users.noreply.github.com>
2024-12-02 18:49:02 +1000

    Constant chars - what could go wrong?

--- day01/main.cpp
+++ day01/main.cpp
@@ -1,16 +1,19 @@
 #include "./main.h"
+#include "./inputs.h"
 
 #include <iostream>
 #include <string_view>
 #include <fstream>
 #include <vector>
+#include <sstream>
 
 namespace day01
 {
 
     int main()
     {
-        std::ifstream input_file("./day01/puzzle_input.txt");
+        std::string test_input(day01inputs::puzzle_input);
+        std::stringstream input_file(test_input);
 
         std::vector<long> left_list(6);
         std::vector<long> right_list(6);
